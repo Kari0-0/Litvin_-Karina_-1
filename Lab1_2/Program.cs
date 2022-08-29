734f774c67ebaaa90242cd43f4b979c49343aec3
Kari0-0 <112347650+Kari0-0@users.noreply.github.com>
2022-08-29 19:01:06 +0300

    add lab1(1,2,3)

---
+++ Lab1_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> d = new Dictionary<string, double>();
+
+            d.Add("item1", 45.50);
+            d.Add("item2", 35);
+            d.Add("item3", 41.30);
+            d.Add("item4", 55);
+            d.Add("item5", 24);
+
+            Dictionary<string, double> newD = d.OrderByDescending(x => x.Value).Take(3).ToDictionary(a => a.Key, b => b.Value);
+            foreach (var item in newD)
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
+            string JSON = JsonConvert.SerializeObject(newD, Formatting.Indented);
+
+            string fileName = @"file[2].json";
+
+            File.Create(fileName).Close();
+            File.WriteAllText(fileName, JSON);
+
+        }
+    }
+}
